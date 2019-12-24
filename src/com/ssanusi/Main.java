bce20f563de71376d7a3258c4db09455954703bc
Sulaiman Sanusi <sulaimansanusi@Sulaimans-MacBook-Pro-2.local>
2019-12-24 22:06:04 +0100

    Mortgage Calculator

--- src/com/ssanusi/Main.java
+++ src/com/ssanusi/Main.java
@@ -4,29 +4,69 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
-
+    final static byte MONTH_IN_YEARS = 12;
+    final static byte PERCENT = 100;
     public static void main(String[] args) {
-	    final byte MONTH_IN_YEARS = 12;
-	    final byte PERCENT = 100;
+         int principal = (int)readNumber("Principal ($1k - $1M): ", 1000, 1_000_000);
+         float annualInterestRate = (float)readNumber("Annual Interest Rate: ",0,30);
+         byte years = (byte)readNumber("Period (Years): ", 1,30);
 
-        Scanner scanner = new Scanner(System.in);
+        printMortgage(principal, annualInterestRate, years);
+        printPaymentSchedule(principal, annualInterestRate, years);
+    }
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+    public static void printMortgage(int principal, float annualInterestRate, byte years) {
+        double mortgage = calculateMortgage(principal, annualInterestRate, years);
+        String result = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println();
+        System.out.println("Mortgage");
+        System.out.println("--------");
+        System.out.println("Mortgage: "+ result);
+    }
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterestRate = scanner.nextFloat();
+    public static void printPaymentSchedule(int principal, float annualInterestRate, byte years) {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("---------------- ");
+        for (short month = 0; month <= years * MONTH_IN_YEARS; month++) {
+            double balance = calculateBalance(principal, annualInterestRate, years, month);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+    }
+
+    public static double calculateMortgage(int principal, float annualInterestRate, int years) {
+        short numberOfPayment = (short)(years * MONTH_IN_YEARS);
+        float monthlyInterestRate = annualInterestRate / PERCENT / MONTH_IN_YEARS;
+        return principal *  (monthlyInterestRate * Math.pow(1 + monthlyInterestRate,numberOfPayment))
+                    / (Math.pow(1+ monthlyInterestRate, numberOfPayment) - 1);
+    }
+
+    public static double calculateBalance(int principal, float annualInterestRate, int years, short numberOfPaymentMade){
+        short numberOfPayment = (short)(years * MONTH_IN_YEARS);
         float monthlyInterestRate = annualInterestRate / PERCENT / MONTH_IN_YEARS;
 
-        System.out.println("Period (Years): ");
-        byte period = scanner.nextByte();
-        int numberOfPayment = period * MONTH_IN_YEARS;
+        double balance = principal
+                * (Math.pow(1 + monthlyInterestRate,numberOfPayment) - Math.pow(1 + monthlyInterestRate, numberOfPaymentMade))
+                / (Math.pow(1 + monthlyInterestRate, numberOfPayment) - 1);
 
-
-        double mortgage = principal *  (monthlyInterestRate * Math.pow(1 + monthlyInterestRate,numberOfPayment))
-                / (Math.pow(1+ monthlyInterestRate, numberOfPayment) - 1);
-        String result = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: "+ result);
-
+        return balance;
     }
+
+    public static double readNumber(String prompt, double min, double max){
+        double value;
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.print(prompt);
+            value = scanner.nextFloat();
+            if(value >= min && value <= max)
+                break;
+            System.out.println("Enter A Value between "+min+" and "+max+":");
+        }
+        return value;
+    }
+
+
 }
+
+
+
